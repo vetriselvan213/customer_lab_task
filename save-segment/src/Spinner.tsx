796f47b2dc6d00ffa29e5f0336a57f11779f2355
s vetriselvan <122189457+vetriselvan213@users.noreply.github.com>
2025-10-24 10:55:34 +0530

    Add Spinner component for loading indication

---
+++ save-segment/src/Spinner.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import "./App.css";
+
+const Spinner: React.FC = () => {
+  return (
+    <div className="spinner-wrapper">
+      <div className="spinner" />
+    </div>
+  );
+};
+
+export default Spinner;
